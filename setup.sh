64d329f3cabca4030fae3a6bffd95fea06b2810b
Emma DE BANVILLE <ede-banv@e2r4p8.42.fr>
2021-03-01 12:21:01 +0100

    root files

--- setup.sh
+++ setup.sh
@@ -2,14 +2,18 @@
 
 #nettoyer tt pour actualiser:
 #kuberctl delete --all {nom du service}
+#kuberctl delete --all nginx
+#kuberctl delete --all wordpress
+#kuberctl delete --all phpmyadmin
+#kuberctl delete --all mysql
+#kuberctl delete --all influxdb
+#kuberctl delete --all grafana
+#kuberctl delete --all ftps
 
-#if pas installe
-	#installer kontrakt
-	#driver=none
 
 #if minikube est pas dj start:
-	#start minikube
-	#stop le nginx par defait de la vm
+	#minikube start --driver=docker
+	#stop le nginx par defaut de la vm (ou pas)
 	#sudo chown (et on remplace $HOME/.kube par $HOME/.minikube)
 
 #installer load balancer (metallb)
@@ -19,13 +23,19 @@
 #echo "IP :" IP (on affiche l'IP du LB pr etre sur que ca s'est bien fait)
 
 #docker build ./srcs/{norm du service}
-#(pour chaque service)
+#docker build ./srcs/nginx
+#docker build ./srcs/wordpress
+#docker build ./srcs/phpmyadmin
+#docker build ./srcs/mysql
+#docker build ./srcs/influxdb
+#docker build ./srcs/grafana
+#docker build ./srcs/ftps
 
-#creer DB et user pr wordpress et host mysql
+#creer DB et user pr wordpress et host mysql (ou pas)
 
-#creer un autre secret
+#creer un autre secret (ou pas) si variable env , sinon + simple
 
 #on creer les services:
-#kuberctl create -f srcs/{nom du service}.yaml
+#kubectl create -f srcs/{nom du service}.yaml
 
 #sudo minikube dashboard (ajouter '&' pour que ca run en background ->moins visuel)
