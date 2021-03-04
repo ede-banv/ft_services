c0b82cb4a01dc68c8811a78f875d57af44d5c3fe
user42 <ede-banv@student.42.fr>
2021-03-04 17:25:56 +0100

    some changes, some comments, more reading to do

---
+++ srcs/wordpress/wp-config.php
@@ -0,0 +1,8 @@
+<?php
+
+#https://wiki.alpinelinux.org/wiki/WordPress
+define( 'DB_NAME', 'wordpress' );
+define( 'DB_USER', 'ede-banv' );
+define( 'DB_PASSWORD', 'owowhatsthis' );
+define( 'DB_HOST', 'localhost' );
+$table_prefix = 'wp_';
