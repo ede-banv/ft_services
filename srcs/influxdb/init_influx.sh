e7e790d6cb9e8e9d2ba1819343856a5405cffe66
user42 <ede-banv@student.42.fr>
2021-03-19 14:10:58 +0100

    influxdb works; to do: install telegraf in all containers and create inputs for influxdb

--- srcs/influxdb/init_influx.sh
+++ srcs/influxdb/init_influx.sh
@@ -4,8 +4,8 @@ sed s/__DB_NAME__/$IDB_NAME/g /etc/telegraf.conf -i
 sed s/__DB_USER__/$IDB_USER/g /etc/telegraf.conf -i
 sed s/__DB_PASSWORD__/$IDB_PASSWORD/g /etc/telegraf.conf -i
 
-rc-service telegraf start
 rc-service influxdb start
+rc-service telegraf start
 
 echo "CREATE DATABASE ${IDB_NAME}" | influx
 echo "CREATE USER ${IDB_USER} WITH PASSWORD '${IDB_PASSWORD}' WITH ALL PRIVILEGES" | influx
