00d58d7fce9a930042a62a66718465acf1c8064a
user42 <ede-banv@student.42.fr>
2021-03-23 17:16:25 +0100

    ftps fixes starting, only list eror left, repeated

--- srcs/ftps/ftpsstart.sh
+++ srcs/ftps/ftpsstart.sh
@@ -4,7 +4,7 @@ sed s/__DB_NAME__/$IDB_NAME/g /etc/telegraf.conf -i
 sed s/__DB_USER__/$IDB_USER/g /etc/telegraf.conf -i
 sed s/__DB_PASSWORD__/$IDB_PASSWORD/g /etc/telegraf.conf -i
 
-rc-service vsftpd restart
+rc-service vsftpd start
 rc-service telegraf start
 
 tail -f /dev/null
