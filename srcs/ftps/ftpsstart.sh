c7aad12df2686439cabe21acb5db337fdb41d19e
user42 <ede-banv@student.42.fr>
2021-03-23 18:21:49 +0100

    fuck ftps

--- srcs/ftps/ftpsstart.sh
+++ srcs/ftps/ftpsstart.sh
@@ -4,7 +4,8 @@ sed s/__DB_NAME__/$IDB_NAME/g /etc/telegraf.conf -i
 sed s/__DB_USER__/$IDB_USER/g /etc/telegraf.conf -i
 sed s/__DB_PASSWORD__/$IDB_PASSWORD/g /etc/telegraf.conf -i
 
-rc-service vsftpd start
+#rc-service vsftpd start
+vsftpd /etc/vsftpd/vsftpd.conf
 rc-service telegraf start
 
 tail -f /dev/null
