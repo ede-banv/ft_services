0df67aa28a016ba67f4703e3c4dc993f80c8690c
user42 <ede-banv@student.42.fr>
2021-03-23 14:52:11 +0100

    mli mariage

--- srcs/grafana/init_grf.sh
+++ srcs/grafana/init_grf.sh
@@ -1,17 +1,21 @@
 #!/bin/sh
 
-sed s/__DB_NAME__/$IDB_NAME/g /usr/share/grafana/conf/provisioning/datasources/database.yaml -i
-sed s/__DB_USER__/$IDB_USER/g /usr/share/grafana/conf/provisioning/datasources/database.yaml -i
-sed s/__DB_PASSWORD__/$IDB_PASSWORD/g /usr/share/grafana/conf/provisioning/datasources/database.yaml -i
+#sed s/__DB_NAME__/$IDB_NAME/g /usr/share/grafana/conf/provisioning/datasources/database.yaml -i
+#sed s/__DB_USER__/$IDB_USER/g /usr/share/grafana/conf/provisioning/datasources/database.yaml -i
+#sed s/__DB_PASSWORD__/$IDB_PASSWORD/g /usr/share/grafana/conf/provisioning/datasources/database.yaml -i
 
-sed s/__GRAF_USER__/$IDB_USER/g /etc/grafana.ini -i
-sed s/__GRAF_PASS__/$IDB_PASSWORD/g /etc/grafana.ini -i
+#sed s/__GRAF_USER__/$IDB_USER/g /etc/grafana.ini -i
+#sed s/__GRAF_PASS__/$IDB_PASSWORD/g /etc/grafana.ini -i
 
 sed s/__DB_NAME__/$IDB_NAME/g /etc/telegraf.conf -i
 sed s/__DB_USER__/$IDB_USER/g /etc/telegraf.conf -i
 sed s/__DB_PASSWORD__/$IDB_PASSWORD/g /etc/telegraf.conf -i
 
-rc-service grafana start
-rc-service telegraf start
+sed "s/;admin_user = admin/admin_user = $IDB_USER/g" /usr/share/grafana/conf/custom.ini -i
+sed "s/;admin_password = admin/admin_password = $IDB_PASSWORD/g" /usr/share/grafana/conf/custom.ini -i
 
-tail -f /dev/null
+#rc-service grafana start
+#rc-service telegraf start
+#tail -f /dev/null
+telegraf &
+grafana-server --homepath=/usr/share/grafana/ --config=/usr/share/grafana/conf/custom.ini >/dev/null 2>&1
