beddd5e82d8080e4fc197a5f16ecc417c1cfda8d
user42 <ede-banv@student.42.fr>
2021-03-23 15:29:41 +0100

    les cokins ont reussi, nique ta mere grafana

--- srcs/nginx/run.sh
+++ srcs/nginx/run.sh
@@ -4,5 +4,5 @@ sed s/__DB_NAME__/$IDB_NAME/g /etc/telegraf.conf -i
 sed s/__DB_USER__/$IDB_USER/g /etc/telegraf.conf -i
 sed s/__DB_PASSWORD__/$IDB_PASSWORD/g /etc/telegraf.conf -i
 
-telegraf &
+rc-service telegraf start
 nginx -g 'daemon off;'
