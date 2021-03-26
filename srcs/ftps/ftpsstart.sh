729965f0735d121fcbca412ae6f5c6eb14110621
user42 <ede-banv@student.42.fr>
2021-03-26 16:42:02 +0100

    clean of files + advancement on livenessprobe commands. to do: fix liveness w grafana, influxdb and pma, check if the crashloop backoffs are because of the services not running or because of the lp command

--- srcs/ftps/ftpsstart.sh
+++ srcs/ftps/ftpsstart.sh
@@ -4,8 +4,8 @@ sed s/__DB_NAME__/$IDB_NAME/g /etc/telegraf.conf -i
 sed s/__DB_USER__/$IDB_USER/g /etc/telegraf.conf -i
 sed s/__DB_PASSWORD__/$IDB_PASSWORD/g /etc/telegraf.conf -i
 
-#rc-service vsftpd start
-vsftpd /etc/vsftpd/vsftpd.conf
+rc-service vsftpd start
+#vsftpd /etc/vsftpd/vsftpd.conf
 rc-service telegraf start
 
 tail -f /dev/null
